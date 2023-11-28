ae9ab3e732a459071599d2b3790596b906bb6a83
bindo <trovato.stefano04@gmail.com>
2023-11-28 09:58:07 +0100

    Commit 2: Creazione della classe Bevanda, HashMap lista prodotti cambiato da <String, Float> a <Integer, Bevanda>

---
+++ src/org/generation/italy/Bevanda.java
@@ -0,0 +1,28 @@
+package org.generation.italy;
+
+public class Bevanda {
+	
+	int codice, quantitàDisponibile;
+	String nome;
+	Float prezzo;
+
+	public Bevanda(int codice, String nome, Float prezzo)
+	{
+		this.codice = codice;
+		this.nome = nome;
+		this.prezzo = prezzo;
+		quantitàDisponibile = 5;
+		
+	}
+	
+	public void eroga() //questo metodo riduce la quantità disponibile di 1
+	{
+		if(quantitàDisponibile > 0)
+			quantitàDisponibile--;
+	}
+	
+	public void carica(int quantità) //questo metodo permette di caricare la quantità desiderata di bevande
+	{
+		
+	}
+}
--- src/org/generation/italy/DistributoreBevande.java
+++ src/org/generation/italy/DistributoreBevande.java
@@ -8,21 +8,22 @@ public class DistributoreBevande {
 	Scanner sc = new Scanner(System.in);
 	
 	float credito, inserisciMoneta; //credito contiene il credito attuale del distributore, inserisciMoneta è la variabile in cui l'utente inserisce i valori delle monete
-	String risposta, scelta; //risposta permette all'utente di aggiungere ulteriori monete, scelta permette di scegliere una delle bevande
+	String risposta; //risposta permette all'utente di aggiungere ulteriori monete
 	boolean check = true, check1 = true;
+	int scelta; //scelta permette di scegliere una delle bevande
 	
-	private HashMap<String, Float> bevande = new HashMap<>(); //questa hashmap contiene tutte le bevande
+	private HashMap<Integer, Bevanda> bevande = new HashMap<>(); //questa hashmap contiene tutte le bevande
 
 	public DistributoreBevande() { //questo costruttore imposta la lista delle bevande e il valore del credito iniziale del distributore
-		bevande.put("Acqua naturale", 0.5f);
-		bevande.put("Acqua frizzante", 0.75f);
-		bevande.put("Limonata", 1f);
-		bevande.put("Coca Cola", 1.5f);
-		bevande.put("Fanta", 1.5f);
-		bevande.put("Sprite", 1.5f);
-		bevande.put("Chinotto", 1.5f);
-		bevande.put("Estathè limone", 1.25f);
-		bevande.put("Estathè pesca", 1.25f);
+		bevande.put(1, new Bevanda(1, "Acqua Naturale", 0.5f));
+		bevande.put(2, new Bevanda(2, "Acqua Frizzante", 0.75f));
+		bevande.put(3, new Bevanda(3, "Coca Cola", 1.5f));
+		bevande.put(4, new Bevanda(4, "Fanta", 1.5f));
+		bevande.put(5, new Bevanda(5, "Sprite", 1.5f));
+		bevande.put(6, new Bevanda(6, "Chinotto", 1.5f));
+		bevande.put(7, new Bevanda(7, "Limonata", 1.25f));
+		bevande.put(8, new Bevanda(8, "Estathè Pesca", 1.25f));
+		bevande.put(9, new Bevanda(9, "Estathè Limone", 1.25f));
 		credito = 0;
 	}
 
@@ -52,19 +53,19 @@ public class DistributoreBevande {
 	
 	public void erogaBevanda() {
 		
-		for(String s : bevande.keySet())
+		for(Integer bevanda : bevande.keySet())
 		{
-			System.out.println("€"+bevande.get(s)+" - "+s);
+			System.out.println("[0"+bevande.get(bevanda).codice+"]"+" - €"+bevande.get(bevanda).prezzo+" - "+bevande.get(bevanda).nome);
 		}
 		
 		do
 		{
 			System.out.println("\n\nScegli una bevanda da bere:");
-			scelta = sc.nextLine();
+			scelta = Integer.parseInt(sc.nextLine());
 			
 			if(bevande.containsKey(scelta))
 			{
-				System.out.println("\n"+scelta+" costa €"+bevande.get(scelta)+"\n");
+				System.out.println("\n"+bevande.get(scelta).nome+" costa €"+bevande.get(scelta).prezzo+"\n");
 				check = true;
 			}
 			else
@@ -76,15 +77,15 @@ public class DistributoreBevande {
 		
 		do
 		{
-			if(credito < bevande.get(scelta))
+			if(credito < bevande.get(scelta).prezzo)
 			{
 				System.out.println("Il credito attuale non è sufficiente per acquistare la bevanda scelta. (€"+credito+")\n");
 				credito();
 			}
-		} while(credito < bevande.get(scelta));
+		} while(credito < bevande.get(scelta).prezzo);
 		
-		credito = credito-bevande.get(scelta);
-		System.out.println("Hai acquistato un'unità di "+scelta+".\nSono stati detratti €"+bevande.get(scelta)+" dal credito. Il credito attuale è €"+credito);
+		credito = credito-bevande.get(scelta).prezzo;
+		System.out.println("Hai acquistato un'unità di "+bevande.get(scelta).nome+".\nSono stati detratti €"+bevande.get(scelta).prezzo+" dal credito. Il credito attuale è €"+credito);
 	}
 	
 	
